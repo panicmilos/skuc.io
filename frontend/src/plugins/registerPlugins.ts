ecc9e49f9846dfc40364f9c4485ee18a8a215189
Luka Bjelica <bjelicaluka99@gmail.com>
2022-05-25 16:34:41 +0200

    refactor: improved register plugins

--- frontend/src/plugins/registerPlugins.ts
+++ frontend/src/plugins/registerPlugins.ts
@@ -2,10 +2,24 @@ import { Plugin } from '../core/Context';
 
 export const registerPlugins = (registerPlugins: (plugins: Plugin[]) => any) => {
   import('.').then((module: { [key: string]: () => Plugin }) => {
-    const plugins = Object.keys(module).map((pluginKey: string) => {
+    const contextPlugins: Plugin[] = [];
+    const featurePlugins: Plugin[] = [];
+    const sidebarPlugins: Plugin[] = [];
+    Object.keys(module).forEach((pluginKey: string) => {
       const getPluginDefinition = module[pluginKey];
-      return getPluginDefinition();
+      const plugin = getPluginDefinition();
+      if(plugin.type === 'SidebarPlugin')
+        sidebarPlugins.push(plugin);
+      if(plugin.type === 'ContextPlugin')
+        contextPlugins.push(plugin);
+      if(plugin.type === 'FeaturePlugin')
+        featurePlugins.push(plugin);
     });
-    registerPlugins(plugins);
+
+    registerPlugins([
+      ...contextPlugins,
+      ...featurePlugins,
+      ...sidebarPlugins
+    ]);
   });
 }
