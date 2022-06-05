d703520a6e7292dde4b9570a8a862be88ee21fb2
Luka Serbedzija <jovluk@hotmail.com>
2022-06-05 12:18:04 +0200

    feat: started working on templates.

---
+++ backend/skuc-io-app/src/main/java/skuc/io/skucioapp/controllers/RuleTemplatesController.java
@@ -0,0 +1,33 @@
+package skuc.io.skucioapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import skuc.io.skucioapp.api_contracts.requests.Templates.CreateTemplateRequest;
+import skuc.io.skuciocore.models.csm.template.RuleTemplate;
+import skuc.io.skuciocore.services.RuleTemplateService;
+
+@RestController
+@RequestMapping("templates")
+public class RuleTemplatesController {
+
+    private final RuleTemplateService _ruleTemplateService;
+
+    @Autowired
+    public RuleTemplatesController(RuleTemplateService ruleTemplateService) {
+        _ruleTemplateService = ruleTemplateService;
+    }
+
+    @PostMapping("{groupId}/ruleTemplates")
+    public ResponseEntity<RuleTemplate> createTemplate(@PathVariable String groupId, @RequestBody CreateTemplateRequest createTemplateRequest) {
+        var ruleTemplate = _ruleTemplateService.create(groupId, createTemplateRequest.getName(), createTemplateRequest.getParameters(),
+            createTemplateRequest.getWhen(), createTemplateRequest.getThen());
+        
+        return ResponseEntity.ok(ruleTemplate);
+    }
+}
---
+++ backend/skuc-io-core/src/main/java/skuc/io/skuciocore/persistence/RuleTemplateRepository.java
@@ -0,0 +1,35 @@
+package skuc.io.skuciocore.persistence;
+
+import org.springframework.stereotype.Repository;
+
+import skuc.io.skuciocore.models.csm.template.RuleTemplate;
+
+@Repository
+public class RuleTemplateRepository extends CrudRepository<RuleTemplate> {
+    
+    public RuleTemplateRepository() {
+        super(RuleTemplate.class);
+    }
+
+    public RuleTemplate getByGroupAndName(String groupId, String name) {
+        try (var session = getSession()) {
+            return session.query(concreteClass)
+                .whereEquals("groupId", groupId)
+                .whereEquals("name", name)
+                .firstOrDefault();
+        }
+    }
+
+    @Override
+    public void update(RuleTemplate ruleTemplate) {
+        try (var session = getSession()) {
+            var existingRuleTemplate = session.load(concreteClass, ruleTemplate.getId());
+
+            existingRuleTemplate.setName(ruleTemplate.getName());
+            existingRuleTemplate.setParameters(ruleTemplate.getParameters());
+            existingRuleTemplate.setTemplate(ruleTemplate.getTemplate());
+
+            session.saveChanges();
+        } 
+    }
+}
